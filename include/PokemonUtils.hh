c88c6f1e69892b59b3bd8074fc3386aaf629f9f9
Ensis <pierre.ecormier@yahoo.fr>
2014-06-27 21:13:29 +0200

    display map and player x/y

--- include/PokemonUtils.hh
+++ include/PokemonUtils.hh
@@ -3,11 +3,16 @@
 
 #include	<stdint.h>
 
+#define		WRAM_OFFSET	0x2000000
+#define		IRAM_OFFSET	0x3000000
 #define		ROM_OFFSET	0x8000000
+
 #define		BANK_PTR	0x3526A8
 #define		NAMES_PTR	0x245EE0
 #define		PTEAM_PTR	0x024284
 #define		ETEAM_PTR	0x02402C
+#define		CURR_BANK_PTR	0x031DBC
+#define		CURR_MAP_PTR	0x031DBD
 
 char		pokeCharsetToAscii(uint8_t c);
 
--- src/main.cpp
+++ src/main.cpp
@@ -12,8 +12,9 @@ void		doLoop()
   Team		&eTeam = data.playerTeam();
   int		step = 0;
 
+  printf("%p\n", workRAM);
   while (emulating) {
-    if (++step % 50 == 0)
+    if (++step % 40 == 0)
       {
 	data.update();
 	printf("\033[2J\033[0;0H");
@@ -22,11 +23,22 @@ void		doLoop()
 	    printf("%s's (%d xp) %s", pTeam[i].getOtName(), pTeam[i].getXP(), data.name(pTeam[i].getSpecies()));
 	    printf("\tvs\t%s\n", eTeam[i].getNick());
 	  }
-	World::Map &m = data.world().getMap(3, 0);
+	uint8_t		bank = *((uint8_t *) (workRAM + 0x031DBC));
+	uint8_t		map = *((uint8_t *) (workRAM + 0x031DBD));
+	World::Map	&m = data.world().getMap(bank, map);
+	uint32_t	offset = *((uint32_t *) (internalRAM + 0x05008)) - 0x2000000;
+	uint16_t	px = *((uint16_t *) (workRAM + offset));
+	uint16_t	py = *((uint16_t *) (workRAM + offset + 2));
 	for (int y = 0; y < m.height; y++)
 	  {
 	    for (int x = 0; x < m.width; x++)
-	      printf("%02x ", m[y][x]);
+	      {
+		if (x == px && y == py)
+		  printf("\033[31m");
+		printf("%02x ", m[y][x]);
+		if (x == px && y == py)
+		  printf("\033[0m");
+	      }
 	    printf("\n");
 	  }
       }
