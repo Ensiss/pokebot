e15917b2ee65c932bb742ab0ba3d6db087b7bc8c
Ensis <pierre.ecormier@yahoo.fr>
2014-07-07 14:42:48 +0200

    add: move names

--- include/Data.hh
+++ include/Data.hh
@@ -20,6 +20,8 @@ public:
 public:
   const std::vector<char *>	&names() const { return (_names); }
   char		*name(uint8_t i) const { return (i < _names.size() ? _names[i] : NULL); }
+  const std::vector<char *>	&moveNames() const { return (_moveNames); }
+  char		*moveName(uint8_t i) const { return (i < _moveNames.size() ? _moveNames[i] : NULL); }
   Team		&playerTeam() { return (_pteam); }
   Team		&enemyTeam() { return (_eteam); }
   World		&world() { return (_world); }
@@ -27,10 +29,12 @@ public:
 
 private:
   void		_loadNames();
+  void		_loadMoveNames();
 
 private:
   // Static
   std::vector<char*>	_names;
+  std::vector<char*>	_moveNames;
   World			_world;
   // Dynamic*
   Player	_player;
--- src/Data.cpp
+++ src/Data.cpp
@@ -5,6 +5,7 @@ Data::Data()
     _eteam((uint64_t) (workRAM + ETEAM_PTR))
 {
   _loadNames();
+  _loadMoveNames();
 }
 
 Data::~Data()
@@ -28,9 +29,25 @@ void		Data::_loadNames()
     {
       _names.push_back(new char[11]());
       ptr = rom + NAMES_PTR + id * 11;
+      if (ptr[0] == 0xAE && ptr[1] == 0xFF)
+	break;
       for (int i = 0; i < 11; i++)
 	_names[id][i] = (i < 10 && ptr[i] != 0xFF) * pokeCharsetToAscii(ptr[i]);
-      if (_names[id][0] == '-' && _names[id][1] == 0)
+    }
+}
+
+void		Data::_loadMoveNames()
+{
+  uint8_t	*ptr;
+  int		len = 13;
+
+  for (int id = 0; 1; id++)
+    {
+      _moveNames.push_back(new char[len]());
+      ptr = rom + MOVE_NAMES_PTR + id * len;
+      if (ptr[0] == 0x00)
 	break;
+      for (int i = 0; i < len; i++)
+	_moveNames[id][i] = (i < len - 1 && ptr[i] != 0xFF) * pokeCharsetToAscii(ptr[i]);
     }
 }
