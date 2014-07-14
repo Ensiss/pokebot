92372e6b3bd5e29e06ec1da2ae1cec17768b8adb
Ensis <pierre.ecormier@yahoo.fr>
2014-07-14 10:57:08 +0200

    fix: cleaning map print and main functions

--- src/main.cpp
+++ src/main.cpp
@@ -10,8 +10,11 @@
 #include	"Thumb.hh"
 #include	<list>
 
-void		printTeam(Team &pTeam, Team &eTeam, Data &data)
+void		printTeam(Data &data)
 {
+  Team		&pTeam = data.playerTeam();
+  Team		&eTeam = data.enemyTeam();
+
   for (int i = 0; i < 6; i++)
     {
       const PokemonData	&p = pTeam[i];
@@ -19,18 +22,17 @@ void		printTeam(Team &pTeam, Team &eTeam, Data &data)
 
       if (!p.getSpecies() && !eTeam[i].getSpecies())
 	continue;
-      printf("%s's (%d xp) %s", p.getOtName(), p.getXP(), sp.getName());
-      printf("\tvs\t%s\n", eTeam[i].getNick());
-      printf("Type  : %s", data.type(sp.getType(0)));
+      printf("%s's %s", p.getOtName(), sp.getName());
+      printf("(%s", data.type(sp.getType(0)));
       if (sp.getType(1) != sp.getType(0))
 	printf("/%s", data.type(sp.getType(1)));
-      printf("\n");
+      printf(")\tvs\t%s\n", eTeam[i].getNick());
       for (int m = 0; m < 4; m++)
 	{
 	  const Move	&move = data.move(p.getMove(m));
 	  float		eff = data.typeEffectiveness(move, data.species(eTeam[i].getSpecies()));
 
-	  printf("Move %d: %s(%s) -> ", m, move.getName(), data.type(move.getType()));
+	  printf("\tMove %d: %s(%s) -> ", m, move.getName(), data.type(move.getType()));
 	  printf(" (Power: %d, Accuracy: %d, %d/%d PP",
 		 move.getPower(), move.getAccuracy(), p.getPP(m), move.getPP());
 	  printf(", Effectiveness:%.2f)\n", eff);
@@ -38,57 +40,59 @@ void		printTeam(Team &pTeam, Team &eTeam, Data &data)
     }
 }
 
+void		printMap(Data &data, Action &action)
+{
+  Player	&p = data.player();
+  World::Map	&m = data.world()[p.getBank()][p.getMap()];
+  World::Path	*path = action.movement().getPath();
+
+  for (int y = 0; y < m.height; y++)
+    {
+      for (int x = 0; x < m.width; x++)
+	{
+	  bool	node = false;
+	  bool	ppos = (y == p.getY() && x == p.getX());
+	  for (int a = 0; path && !node && a < path->size(); a++)
+	    if (x == (*path)[a]->x && y == (*path)[a]->y)
+	      node = true;
+	  for (int a = 0; !node && a < m.nbWarps; a++)
+	    if (m.warps[a].x == x && m.warps[a].y == y)
+	      node = true;
+	  printf("\033[1;%d;%dm%02x \033[0m", node && !ppos ? 47 : 40,
+		 m.getMatterColor(m[y][x].status, ppos), m[y][x].status);
+	}
+      printf("\n");
+    }
+}
+
 void		doLoop()
 {
   Data		data;
   Action	action(data);
-  Team		&pTeam = data.playerTeam();
-  Team		&eTeam = data.enemyTeam();
   int		step = 0;
-  World::Path	*path = NULL;
 
-  while (emulating) {
-    if (++step < 900)
-      {
-	sdlSetButton(KEY_BUTTON_AUTO_A, step < 899);
-	sdlSetButton(KEY_BUTTON_SPEED, step < 899);
-      }
-    else
-      {
-	data.update();
-	action.update();
-	path = action.movement().getPath();
-	if (step % 20 == 0)
-	  {
-	    printf("\033[2J\033[0;0H");
-	    printTeam(pTeam, eTeam, data);
+  while (emulating)
+    {
+      if (++step < 900)
+	{
+	  sdlSetButton(KEY_BUTTON_AUTO_A, step < 899);
+	  sdlSetButton(KEY_BUTTON_SPEED, step < 899);
+	}
+      else
+	{
+	  data.update();
+	  action.update();
+	  if (step % 20 == 0)
+	    {
+	      printf("\033[2J\033[0;0H");
+	      printTeam(data);
+	      printMap(data, action);
+	    }
+	}
 
-	    World::Map	&m = data.world()[data.player().getBank()][data.player().getMap()];
-	    for (int y = 0; y < m.height; y++)
-	      {
-	    	for (int x = 0; x < m.width; x++)
-	    	  {
-	    	    bool	node = false;
-	    	    bool	ppos = (y == data.player().getY() && x == data.player().getX());
-	    	    for (int a = 0; path && !node && a < path->size(); a++)
-	    	      if (x == (*path)[a]->x && y == (*path)[a]->y)
-	    		node = true;
-		    for (int a = 0; !node && a < m.nbWarps; a++)
-		      if (m.warps[a].x == x && m.warps[a].y == y)
-			node = true;
-		    printf("\033[1;%d;%dm%02x \033[0m", node && !ppos ? 47 : 40,
-			   m.getMatterColor(m[y][x].status, ppos), m[y][x].status);
-		    printf("\033[1;%d;%dm%02x \033[0m", node && !ppos ? 47 : 40,
-			   m.getMatterColor(m[y][x].status, ppos), m[y][x].attr->behavior);
-	    	  }
-	    	printf("\n");
-	      }
-	  }
-      }
-
-    emulator.emuMain(emulator.emuCount);
-    sdlPollEvents(data, action);
-  }
+      emulator.emuMain(emulator.emuCount);
+      sdlPollEvents(data, action);
+    }
 }
 
 int		main(int ac, char **av)
