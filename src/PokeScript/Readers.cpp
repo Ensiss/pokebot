7387d8a6172c0c9ef4ddad0e8d3f514827ebbe39
Ensis <pierre.ecormier@yahoo.fr>
2014-11-10 13:58:38 +0000

    add: merge Args and Readers files

--- src/PokeScript/Readers.cpp
+++ src/PokeScript/Readers.cpp
@@ -16,3 +16,24 @@ std::map<std::string, Script::ParamReader> Script::_readers = {
   {"word/var", &Script::_readWord},
   {"byte/var", &Script::_readWord}
 };
+
+uint32_t	Script::_readByte()
+{
+  uint8_t	ret = *((uint8_t *) (_ptr + _pc));
+  _pc += 1;
+  return (ret);
+}
+
+uint32_t	Script::_readWord()
+{
+  uint16_t	ret = *((uint16_t *) (_ptr + _pc));
+  _pc += 2;
+  return (ret);
+}
+
+uint32_t	Script::_readDword()
+{
+  uint32_t	ret = *((uint32_t *) (_ptr + _pc));
+  _pc += 4;
+  return (ret);
+}
