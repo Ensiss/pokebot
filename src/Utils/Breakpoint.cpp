3f273c806e5e5430ad0fc66e09af18e7dfd25e54
Ensis <pierre.ecormier@yahoo.fr>
2014-12-05 19:11:01 +0000

    fix: update npc script after talking

--- src/Utils/Breakpoint.cpp
+++ src/Utils/Breakpoint.cpp
@@ -2,19 +2,29 @@
 #include	"Script.hh"
 #include        "VM.hh"
 
+static Script   *_getScript()
+{
+  uint8_t       idx = gbaMem<uint8_t>(0x03005074);
+  if (!idx)
+    return (NULL);
+  uint8_t       evt = Data::data->overWorld(idx).getEventNb();
+  return (Script::getPerson(evt - 1));
+}
+
 void            debuggerBreakOnWrite(u32 address, u32 oldvalue, u32 value, int size, int t)
 {
+  Script        *sc = _getScript();
+
   // End of script execution
   if (oldvalue && !value)
     {
       VM::vm->updateAndCallback();
+      if (sc)
+        VM::vm->execCountNewVisits(sc);
       return;
     }
-  uint8_t       idx = gbaMem<uint8_t>(0x03005074);
-  if (!idx)
+  if (!sc)
     return;
-  uint8_t       evt = Data::data->overWorld(idx).getEventNb();
-  Script        *sc = Script::getPerson(evt - 1);
   std::map<int, Script::Instruction *> &map = sc->getInstructions();
 
   for (std::map<int, Script::Instruction *>::iterator it = map.begin(); it != map.end(); it++)
