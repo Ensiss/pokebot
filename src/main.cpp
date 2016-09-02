74a2006ddfbd17f45884fea8e6e7d62a2242704f
Ensis <pierre.ecormier@yahoo.fr>
2016-09-02 16:42:50 +0200

    add rom validity check for Firered US V1.0

--- src/main.cpp
+++ src/main.cpp
@@ -30,32 +30,48 @@ void		doLoop()
         }
       L.doFunc("onEnterFrame");
       if (++step < 900)
-	{
-	  sdlSetButton(KEY_BUTTON_AUTO_A, step < 899);
-	  sdlSetButton(KEY_BUTTON_SPEED, step < 899);
-	}
+        {
+          sdlSetButton(KEY_BUTTON_AUTO_A, step < 899);
+          sdlSetButton(KEY_BUTTON_SPEED, step < 899);
+        }
       else
-	{
-	  data.update();
+        {
+          data.update();
           Bot::bot.update();
-	  if (step == 900)
+          if (step == 900)
             {
               VM::vm->update();
               L.doFunc("onInit");
             }
-	  if (step % Config::getNumber("refreshRate") == 0)
-	    {
+          if (step % Config::getNumber("refreshRate") == 0)
+            {
               if (Config::getNumber("clearOnRefresh"))
                 printf("\033[2J\033[0;0H");
-	      printTeam(data);
-	      printMap(data);
+              printTeam(data);
+              printMap(data);
               L.doFunc("onRefresh");
-	    }
-	}
+            }
+        }
 
       emulator.emuMain(emulator.emuCount);
       sdlPollEvents(data, Bot::bot);
     }
+}
+
+static bool     isROMValid()
+{
+  const char *err = NULL;
+
+  if (strncmp(gbaPtr<char *>(0x080000A0), "POKEMON FIRE", 12))
+    err = "the loaded game is not Pokemon FireRed";
+  else if (strncmp(gbaPtr<char *>(0x080000AC), "BPRE", 4))
+    err = "the loaded game is not the english version";
+  else if (gbaMem<uint8_t>(0x080000BC) != 0x00)
+    err = "the game version is not V1.0";
+
+  if (err)
+    fprintf(stderr, "Error: %s. Check Readme.md to see what the right rom is\n", err);
+  return !err;
 }
 
 int		main(int ac, char **av)
@@ -66,12 +82,16 @@ int		main(int ac, char **av)
       return (1);
     }
   initVBAM(ac, av);
-  // Break when script engine PC is modified
-  debuggerDoString("bpw 03000EB8 4");
-  Data::data = new Data();
-  VM::vm = new VM();
-  L.init();
-  doLoop();
+
+  if (isROMValid())
+    {
+      // Break when script engine PC is modified
+      debuggerDoString("bpw 03000EB8 4");
+      Data::data = new Data();
+      VM::vm = new VM();
+      L.init();
+      doLoop();
+    }
   destroyVBAM();
   return (0);
 }
